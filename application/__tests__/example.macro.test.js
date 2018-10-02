df9e36c8ef02498aa297757a6deffc5ae22c243f
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2018-10-02 14:05:37 +0100

    WIP on example macro test, introduced a template factory

--- application/__tests__/example.macro.test.js
+++ application/__tests__/example.macro.test.js
@@ -1,24 +1,78 @@
 // 1. Expect example macro to render a header
-/* global DOMParser describe test expect */
 
 const { JSDOM } = require('jsdom')
 const path = require('path')
 const nunjucks = require('jstransformer')(require('jstransformer-nunjucks'))
-let baseDocument = new JSDOM('<!DOCTYPE html><html><head></head><body><div id="exampleContainer"></div></body></html>', { contentType: 'text/html' }).window.document
+
+function isArray (obj) { return Array.isArray(obj) }
+
 const options = {
-  path: path.join(__dirname, '..', 'macros')
+  path: path.join(__dirname, '..', 'macros'),
+  trimBlocks: true,
+  lstripBlocks: true,
+  globals: { pathname: '/__tests__' },
+  filters: { is_array: isArray }
 }
 
-describe('Example macro', () => {
-  test('should render a header with Hello world', () => {
-    const templateString = `
-    {% from "example.macro.njk"  import example %}
-    
-    {{ example({html: 'Hello'}) }}`
+const defaultHeight = 153
 
-    const htmlString = nunjucks.render(templateString, options).body
-    baseDocument.getElementById('exampleContainer').innerHTML = htmlString
-    console.log(baseDocument.body.innerHTML)
-    expect(htmlString).toContain('Hello')
+const { document } = (new JSDOM(
+  '<!DOCTYPE html><html><head></head><body><div id="exampleContainer"></div></body></html>',
+  { contentType: 'text/html' }))
+  .window
+
+let exampleContainer = document.getElementById('exampleContainer')
+
+describe('Example macro', () => {
+
+  const templateFactory = (parameters) => {
+    // ToDo: check if there's a better way of handling the object passed in
+    return `{%- from "example.macro.njk"  import example -%} 
+    {{ example(${JSON.stringify(parameters)})
+    }}`.toString()
+  }
+
+  test('should render an iFrame for the example', () => {
+    const exampleId = 'test'
+    const exampleSrc = path.join('example', options.globals.pathname, exampleId + '.html').toString()
+
+    const templateString = `
+    {%- from "example.macro.njk"  import example -%} 
+    {{ example({html: '${exampleId}.html'}) }}`
+
+    exampleContainer.innerHTML = nunjucks.render(templateString, options).body
+    const exampleFrame = document.getElementById(`${exampleId}_frame`)
+  
+    expect(exampleFrame).not.toBeNull()
+    expect(exampleFrame.tagName.toLowerCase()).toBe('iframe')
+    expect(exampleFrame.name).toBe(`${exampleId}_frame`)
+    expect(parseInt(exampleFrame.height)).toEqual(defaultHeight)
+    expect(exampleFrame.src).toMatch(exampleSrc)
+  })
+
+  test('should set the iframe height when it is supplied', () => {
+    const exampleId = 'test'
+    const frameHeight = 256
+
+    const templateString = `
+    {%- from "example.macro.njk"  import example -%} 
+    {{ example({
+      html: '${exampleId}.html', 
+      height: ${frameHeight}}) 
+    }}`
+
+    exampleContainer.innerHTML = nunjucks.render(templateString, options).body
+    const exampleFrame = document.getElementById(`${exampleId}_frame`)
+    expect(parseInt(exampleFrame.height)).toBe(256)
+  })
+
+  test('should enable multiPage when the html paramater property is an Array ', () => {
+
+    const parameters = { html: ['example1.html', 'example2.html'] }
+    const templateString = templateFactory(parameters)
+
+    exampleContainer.innerHTML = nunjucks.render(templateString, options).body
+    const exampleFrame = document.getElementById('example1_frame')
+    expect(exampleFrame.name).toBe('example1_frame')
   })
 })
