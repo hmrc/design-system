3d4bef53721287c5706b0e69d8f7cc3c28579dc4
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-10-08 14:53:26 +0100

    Changed webserver tech to avoid vulnerabilites, removed livereload until we have a strong enough use-case for it.

--- tasks/gulp/serve.js
+++ tasks/gulp/serve.js
@@ -1,19 +1,10 @@
 const gulp = require('gulp')
-const webserver = require('gulp-webserver')
+const connect = require('gulp-connect')
 const pathFromRoot = require('./util').pathFromRoot
 
 gulp.task('serve', () => {
-  gulp.src(pathFromRoot('dist'))
-    .pipe(webserver({
-      livereload: true,
-      open: false,
-      middleware: (req, res, next) => {
-        if (req.url === '/') {
-          res.writeHead(302, { location: '/index.html' })
-          res.end()
-        } else {
-          next()
-        }
-      }
-    }))
+  connect.server({
+    root: pathFromRoot('dist'),
+    port: process.env.PORT || process.env.port || 3000
+  })
 })
