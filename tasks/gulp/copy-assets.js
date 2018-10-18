78522c305d4f847c69c4c6f497ae31044e7bc9e3
Mat Carey <mat.carey@digital.hmrc.gov.uk>
2018-10-18 09:49:51 +0100

    Fixing copy assets task

--- tasks/gulp/copy-assets.js
+++ tasks/gulp/copy-assets.js
@@ -2,6 +2,6 @@ const gulp = require('gulp')
 const pathFromRoot = require('./util').pathFromRoot
 
 gulp.task('copy-assets', () => gulp
-  .src(pathFromRoot('..', 'node_modules', 'govuk-frontend', 'assets', '**', '*'))
+  .src(pathFromRoot('node_modules', 'govuk-frontend', 'assets', '**', '*'))
   .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
 )
