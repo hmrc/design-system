d0dc8e2f6b27873cac6360213b14c9b767b79c72
Mat Carey <mat@yagni.digital>
2018-10-18 15:57:03 +0100

    Set up build task - npm run build

--- tasks/gulp/copy-assets.js
+++ tasks/gulp/copy-assets.js
@@ -1,7 +1,15 @@
 const gulp = require('gulp')
 const pathFromRoot = require('./util').pathFromRoot
+const gulpSequence = require('gulp-sequence')
 
-gulp.task('copy-assets', () => gulp
+gulp.task('copy-assets:govuk-frontend', () => gulp
   .src(pathFromRoot('node_modules', 'govuk-frontend', 'assets', '**', '*'))
   .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
 )
+
+gulp.task('copy-assets:application', () => gulp
+  .src(pathFromRoot('application', 'assets', '**', '*'))
+  .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
+)
+
+gulp.task('copy-assets', gulpSequence(['copy-assets:govuk-frontend', 'copy-assets:application']))
