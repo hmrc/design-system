59694686bb9944259abb177154ac6aefe495ae67
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2018-10-23 14:08:44 +0100

    Working version of Expandable list taken from GDS Manuals Frontend

--- tasks/gulp/copy-assets.js
+++ tasks/gulp/copy-assets.js
@@ -1,15 +1,24 @@
 const gulp = require('gulp')
+const merge = require('merge-stream')
+const remoteSrc = require('gulp-remote-src')
 const pathFromRoot = require('./util').pathFromRoot
-const gulpSequence = require('gulp-sequence')
 
-gulp.task('copy-assets:govuk-frontend', () => gulp
-  .src(pathFromRoot('node_modules', 'govuk-frontend', 'assets', '**', '*'))
-  .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
-)
+gulp.task('copy-assets', () => {
+  const assets = gulp.src([
+    pathFromRoot('node_modules', 'govuk-frontend', 'assets', '**', '*'),
+    pathFromRoot('application', 'assets', '**', '*')
+  ])
+    .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
 
-gulp.task('copy-assets:application', () => gulp
-  .src(pathFromRoot('application', 'assets', '**', '*'))
-  .pipe(gulp.dest(pathFromRoot('dist', 'assets')))
-)
+  const jquery = gulp.src(pathFromRoot('node_modules', 'jquery', 'dist', '*'))
+    .pipe(gulp.dest(pathFromRoot('dist', 'assets', 'javascripts', 'vendor', 'jquery')))
 
-gulp.task('copy-assets', gulpSequence(['copy-assets:govuk-frontend', 'copy-assets:application']))
+  const collapsible = remoteSrc([
+    'collapsible.js',
+    'collapsible_collection.js',
+    'current_location.js'
+  ], { base: 'https://raw.githubusercontent.com/alphagov/manuals-frontend/master/app/assets/javascripts/modules/' })
+    .pipe(gulp.dest(pathFromRoot('dist', 'assets', 'javascripts', 'vendor', 'govuk')))
+
+  return merge(assets, jquery, collapsible)
+})
