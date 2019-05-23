42d4fee18b3e0865776ab64819a61c3528d97603
Mat Carey <mat@yagni.digital>
2019-05-23 10:49:18 +0100

    Added Language Toggle for examples.

--- application/assets/javascripts/hmrc-design-system.js
+++ application/assets/javascripts/hmrc-design-system.js
@@ -1,10 +1,17 @@
 import common from 'govuk-frontend/common'
 import AppTabs from './components/tabs.js'
+import LanguageSwitchExample from './components/language-switch-example'
 
 var nodeListForEach = common.nodeListForEach
 
 // Initialise tabs
-var $tabs = document.querySelectorAll('[data-module="app-tabs"]')
+var $tabs = document.querySelectorAll('[data-module~="app-tabs"]')
 nodeListForEach($tabs, function ($tabs) {
   new AppTabs($tabs).init()
+})
+
+// Initialise language switch
+var $languageSwitchExamples = document.querySelectorAll('[data-module~="app-language-switch-example"]')
+nodeListForEach($languageSwitchExamples, function ($example) {
+  new LanguageSwitchExample($example).init()
 })
