24605df43135487ee5d6721de19ff7d28d07d784
Jenkins user <jenkins@ip-10-35-64-18.eu-west-2.compute.internal>
2019-06-26 13:13:16 +0000

    publish: Merge pull request #20 from hmrc/PLATUI-64_navigation-titles

--- assets/javascripts/hmrc-design-system.js
+++ assets/javascripts/hmrc-design-system.js
@@ -1492,12 +1492,59 @@
 	  }
 	};
 
+	// Taken from https://github.com/alphagov/govuk-design-system/blob/29b9cf8c30ac1514d16fc97adaf15100e5040f7d/src/javascripts/components/tabs.js
+
 	var nodeListForEach$1 = common.nodeListForEach;
 
+	function LanguageSwitchExample ($module) {
+	  this.$module = $module;
+	  this.$switches = this.$module.querySelectorAll('.app-example__language-switch');
+	  this.$iframe = this.$module.querySelector('[data-module~="app-example-frame"]');
+	  this.currentClassName = 'app-example__language-switch--current';
+	  this.getLanguageClass = function (lang) {
+	    return ['app-example__language-switch--', lang, '-activated'].join('')
+	  };
+	}
+
+	LanguageSwitchExample.prototype.init = function () {
+	  var self = this;
+
+	  if (!this.$module) {
+	    return
+	  }
+
+	  nodeListForEach$1(this.$switches, function ($this) {
+	    $this.addEventListener('click', self.handleClick.bind(self));
+	  });
+	};
+
+	// Close current container on click
+	LanguageSwitchExample.prototype.handleClick = function (event) {
+	  var self = this;
+	  event.preventDefault();
+	  var $target = event.target;
+	  nodeListForEach$1(this.$module.querySelectorAll('.' + this.currentClassName), function ($option) {
+	    $option.classList.remove(self.currentClassName);
+	    $option.querySelector('a').focus();
+	  });
+	  this.$iframe.setAttribute('src', $target.getAttribute('href'));
+	  $target.parentNode.classList.add(this.currentClassName);
+	  this.$module.classList.remove(this.getLanguageClass('en'), this.getLanguageClass('cy'));
+	  this.$module.classList.add(this.getLanguageClass($target.getAttribute('data-lang')));
+	};
+
+	var nodeListForEach$2 = common.nodeListForEach;
+
 	// Initialise tabs
-	var $tabs = document.querySelectorAll('[data-module="app-tabs"]');
-	nodeListForEach$1($tabs, function ($tabs) {
+	var $tabs = document.querySelectorAll('[data-module~="app-tabs"]');
+	nodeListForEach$2($tabs, function ($tabs) {
 	  new AppTabs($tabs).init();
+	});
+
+	// Initialise language switch
+	var $languageSwitchExamples = document.querySelectorAll('[data-module~="app-language-switch-example"]');
+	nodeListForEach$2($languageSwitchExamples, function ($example) {
+	  new LanguageSwitchExample($example).init();
 	});
 
 }());
