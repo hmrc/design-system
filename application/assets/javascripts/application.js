b98a3d388f9ef4833224cb896d4c4162cbf97732
Silas <1029540+silasl@users.noreply.github.com>
2019-07-11 15:10:01 +0100

    PLATUI-8: Replace collapsible pattern with accordian

--- application/assets/javascripts/application.js
+++ application/assets/javascripts/application.js
@@ -6,3 +6,12 @@ jQuery(function ($) {
     return new GOVUK.CollapsibleCollection({ $el: $(this) })
   })
 })
+
+/* document.addEventListener("DOMContentLoaded", function(){
+  'use strict'
+  document.querySelectorAll('.js-collapsible-collection').forEach(function ($el) {
+    console.log('$el', $el);
+    return new GOVUK.CollapsibleCollection({ $el: $el })
+  })
+}); */
+
