b5d1e1d0cb0c0bf7ebb46fd41eb13591b0a9d967
Jenkins user <jenkins@ip-10-35-70-107.eu-west-2.compute.internal>
2019-09-23 10:40:57 +0000

    publish: PLATUI-205: force Accordion to open sections for anchors (#60)

--- assets/javascripts/hmrc-design-system.js
+++ assets/javascripts/hmrc-design-system.js
@@ -6750,6 +6750,39 @@
 	  }, 5000);
 	};
 
+	var linkLocator = '.govuk-link[href^="#"]';
+	var sectionButtonLocator = '.govuk-accordion__section-button';
+
+	function Accordion ($module) {
+	  this.$module = $module;
+	  this.$allAnchorLinks = this.$module.querySelectorAll(linkLocator);
+	  this.$allSectionButtons = this.$module.querySelectorAll(sectionButtonLocator);
+	  this.$mappedSectionButtons = {};
+	}
+
+	Accordion.prototype.init = function () {
+	  if (!this.$module) {
+	    return
+	  }
+	  this.$allAnchorLinks.forEach(this.attachClick.bind(this));
+	  var self = this;
+	  this.$allSectionButtons.forEach(function (button) {
+	    self.$mappedSectionButtons[button.textContent.trim().toLowerCase()] = button.id;
+	  });
+	};
+
+	Accordion.prototype.attachClick = function (link) {
+	  link.addEventListener('click', this.handleClick.bind(this));
+	};
+
+	Accordion.prototype.handleClick = function (evt) {
+	  var section = evt.target.hash[1].trim().toLowerCase();
+	  var button = this.$module.querySelector('#' + this.$mappedSectionButtons[section]);
+	  if (button.getAttribute('aria-expanded') !== 'true') {
+	    button.click();
+	  }
+	};
+
 	// Taken from https://github.com/alphagov/govuk-design-system/blob/29b9cf8c30ac1514d16fc97adaf15100e5040f7d/src/javascripts/components/tabs.js
 
 	var nodeListForEach$1 = common.nodeListForEach;
@@ -9444,6 +9477,12 @@
 	const $copyToClipboardButtons = document.querySelectorAll('[data-module~="copy-to-clipboard"]');
 	nodeListForEach$2($copyToClipboardButtons, $button => {
 	  new CopyToClipboard($button).init();
+	});
+
+	// Initialise temporary accordian workaround
+	const $accordions = document.querySelectorAll('[data-module~="govuk-accordion"]');
+	nodeListForEach$2($accordions, $accordion => {
+	  new Accordion($accordion).init();
 	});
 
 	all.initAll();
