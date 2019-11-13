aa40c4ba0ec56ec1b94711093b9ab343e40aca65
Silas <1029540+silasl@users.noreply.github.com>
2019-11-13 16:27:13 +0000

    Moved gulp to dependencies

--- tasks/gulp/serve.js
+++ tasks/gulp/serve.js
@@ -4,6 +4,14 @@ const connect = require('gulp-connect')
 const pathFromRoot = require('../../util/pathFromRoot')
 const { integrationTestPort } = require('../../constants')
 
+gulp.task('serve:heroku', (done) => {
+  connect.server({
+    root: pathFromRoot('dist'),
+    port: process.env.PORT || process.env.port || 3000
+  })
+  done()
+})
+
 gulp.task('serve', (done) => {
   connect.server({
     root: pathFromRoot('dist'),
