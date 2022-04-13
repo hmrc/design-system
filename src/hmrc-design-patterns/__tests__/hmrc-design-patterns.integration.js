b0c07d1a9561214721d4e3771670608e4add402a
tomshaw1 <tom.shaw1@digital.hmrc.gov.uk>
2022-04-13 12:41:36 +0100

    Updating integration tests to match title changes

--- src/hmrc-design-patterns/__tests__/hmrc-design-patterns.integration.js
+++ src/hmrc-design-patterns/__tests__/hmrc-design-patterns.integration.js
@@ -23,12 +23,12 @@ describe('HMRC design patterns', () => {
 
   it('should have the correct meta title', async () => {
     const title = await page.title()
-    expect(title).toBe('HMRC Design Patterns - Design resources for HMRC – GOV.UK')
+    expect(title).toBe('HMRC design patterns - Design resources for HMRC – GOV.UK')
   })
 
   it('should have the correct page heading', async () => {
     const heading = await page.$eval('h1', el => el.textContent);
-    expect(heading).toBe('HMRC Design Patterns')
+    expect(heading).toBe('HMRC design patterns')
   })
 
   describe('Sub navigation', () => {
