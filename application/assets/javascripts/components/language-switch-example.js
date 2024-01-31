acb92cc55aba4feeec4e1d9ffeb6b76d9d154b07
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2024-01-31 09:50:30 +0000

    PLATUI-2707 - Bump HMRC- and GOVUK-Frontend (#797)
    * PLATUI-2707 update test for templatePath change
    * PLATUI-2707 update templatePaths to pull in govuk-frontend templates from node_modules
    * PLATUI-2707 remove unused ie8 stylesheet reference
    * PLATUI-2707 add capitalisation,  govuk-label to labels
    Co-authored-by: david.ellam <ellamdav@users.noreply.github.com>

--- application/assets/javascripts/components/language-switch-example.js
+++ application/assets/javascripts/components/language-switch-example.js
@@ -1,12 +1,5 @@
 // Taken from https://github.com/alphagov/govuk-design-system/blob/29b9cf8c30ac1514d16fc97adaf15100e5040f7d/src/javascripts/components/tabs.js
 
-import 'govuk-frontend/govuk/vendor/polyfills/Function/prototype/bind'
-import 'govuk-frontend/govuk/vendor/polyfills/Element/prototype/classList'
-import 'govuk-frontend/govuk/vendor/polyfills/Event'
-import common from 'govuk-frontend/govuk/common'
-
-var nodeListForEach = common.nodeListForEach
-
 function LanguageSwitchExample ($module) {
   this.$module = $module
   this.$switches = this.$module.querySelectorAll('.app-example__language-switch a')
@@ -24,7 +17,7 @@ LanguageSwitchExample.prototype.init = function () {
     return
   }
 
-  nodeListForEach(this.$switches, function ($this) {
+this.$switches.forEach(function ($this) {
     $this.addEventListener('click', self.handleClick.bind(self))
   })
 }
@@ -34,7 +27,7 @@ LanguageSwitchExample.prototype.handleClick = function (event) {
   var self = this
   event.preventDefault()
   var $target = event.target
-  nodeListForEach(this.$module.querySelectorAll('.' + this.currentClassName), function ($option) {
+  this.$module.querySelectorAll('.' + this.currentClassName).forEach(function ($option) {
     $option.classList.remove(self.currentClassName)
     $option.querySelector('a').focus()
   })
